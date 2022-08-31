49ac05c32b40d16e9ca484a740c0dee791796fd7
jayomeara <jay@idlewild.industries>
2022-08-31 09:44:57 -0600

    assignment folder create

---
+++ assignments/week 01/Arrays To Do 1/arrays.js
@@ -0,0 +1,15 @@
+// Push front
+// Examples:
+
+// pushFront([5,7,2,3], 8) => [8,5,7,2,3]
+// pushFront([99], 7) => [7,99]
+
+// Pop Front:
+
+// popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
+// popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
+
+// InsertAt:
+
+// insertAt([100,200,5], 2, 311) => [100,200,311,5]
+// insertAt([9,33,7], 1, 42) => [9,42,33,7]
