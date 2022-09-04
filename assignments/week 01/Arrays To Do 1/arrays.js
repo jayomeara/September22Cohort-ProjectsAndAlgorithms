e5671b52cbdcad97a7aa0b6c806e87f057f24b5c
jayomeara <jay@idlewild.industries>
2022-09-04 17:26:59 -0400

    sunday coding work

--- assignments/week 01/Arrays To Do 1/arrays.js
+++ assignments/week 01/Arrays To Do 1/arrays.js
@@ -39,3 +39,12 @@ function insertAt(arr,index,val){
 console.log(insertAt([100,200,5], 2, 311))
 // insertAt([100,200,5], 2, 311) => [100,200,311,5]
 // insertAt([9,33,7], 1, 42) => [9,42,33,7]npm -v
+
+// removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+// removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
+
+// insertAt([1,2,3,4]) => [2,1,4,3]
+// insertAt(["Brendan",true,42]) => [true,"Brendan",42]
+
+// removeDupes([-2,-2,3.14,5,5,10]) => [-2,3.14,5,10]
+// removeDupes([9,19,19,19,19,19,29]) => [9,19,29]
